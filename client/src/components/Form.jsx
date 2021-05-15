73d12f475d384336dfbcff5fe4207fdec1bf27c0
mstefa <mstefanutti24@gmail.com>
2021-05-14 22:20:08 -0300

    conection with metamask or gangache

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,6 +1,51 @@
 import React from 'react'
+import Web3 from 'web3';
+// import Crud from 
 
 export default function Form() {
+
+  let web3;
+  let crud;
+
+  const initWeb3 = () => {
+
+
+    return new Promise((resolve, reject) => {
+
+      // case 1: New metamas is present
+      if (typeof window.ethereum !== 'undefined') {
+        window.ethereum.enable()
+          .them(() => {
+            resolve(
+              new Web3(window.ethereum)
+            )
+          })
+          .catch(e => {
+            reject(e);
+          });
+        return;
+      }
+
+      //case 2: Old metamask is present
+      if (typeof window.web3 !== 'undefined') {
+        return resolve(
+          new Web3(window.web3.currentProvider)
+        );
+      }
+
+      // case 3: no metabas present, just connect to Ganache
+
+      resolve(new Web3('http://localhost:9545'));
+    })
+  
+
+  // const initContract = () => {
+  //   Crud
+  //   return new web3.eth.Contract(
+
+  //   )
+  // }
+  }
   return (
     <div class="container">
       <div class="row">
