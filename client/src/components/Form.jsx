da74d09643fdb400a16c304a6ba6fea29fce253a
mstefa <mstefanutti24@gmail.com>
2021-05-15 12:34:26 -0300

    connect to network via metamask

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,96 +1,138 @@
 import React from 'react'
 import Web3 from 'web3';
-// import Crud from 
+import Crud from '../data.json'
+
+// password password
 
 export default function Form() {
 
+  const [input, setInput] = React.useState({
+    createName: '',
+    readId: null,
+    editId: null,
+    editName: '',
+    deleteId: null,
+  });
+  
+  const handleInputChange = function(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    
+  }
+
   let web3;
   let crud;
+  let accounts = [];
 
-  const initWeb3 = () => {
-
-
-    return new Promise((resolve, reject) => {
-
-      // case 1: New metamas is present
-      if (typeof window.ethereum !== 'undefined') {
-        window.ethereum.enable()
-          .them(() => {
-            resolve(
-              new Web3(window.ethereum)
-            )
-          })
-          .catch(e => {
-            reject(e);
-          });
-        return;
-      }
-
-      //case 2: Old metamask is present
-      if (typeof window.web3 !== 'undefined') {
-        return resolve(
-          new Web3(window.web3.currentProvider)
-        );
-      }
-
-      // case 3: no metabas present, just connect to Ganache
-
-      resolve(new Web3('http://localhost:9545'));
-    })
-  
-
-  // const initContract = () => {
-  //   Crud
-  //   return new web3.eth.Contract(
-
-  //   )
-  // }
+  const initWeb3 = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      console.log('MetaMask is installed!');
+      await window.ethereum.send('eth_requestAccounts');
+      web3 = new Web3(window.ethereum);
+      return true;
+    }
+    web3 = new Web3('http://localhost:9545')
+    return false;
   }
+
+  const initContract = () => {
+    const deploymentKey = Object.keys(Crud.networks)[0];
+    console.log(deploymentKey)
+    console.log(`web3`, web3)
+    crud = new web3.eth.Contract(
+      Crud.abi,
+      Crud
+        .networks[deploymentKey]
+        .address
+    );
+    console.log(`crud`, crud)
+    return true
+  }
+
+  initWeb3()
+    .then(()=> {
+      // console.log('init web3')
+      // console.log(web3)
+      crud = initContract()
+      web3.eth.getAccounts()
+      .then(_accounts => {
+        accounts = _accounts;
+        // console.log(`accounts`, accounts)
+      })
+    })
+    .catch(e => console.error(e.message));
+
+    
+  const handleCreate = (e) => {
+    e.preventDefault();
+    crud.methods.create(input.createName).send({from: accounts[0]})
+    .then(result => {
+      alert("User Create")
+    })
+    .catch(_e => {
+      alert("Something went wrong")
+    });
+  }
+
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
+    <div className="container">
+      {/* <button onClick={()=>ethereum.request({ method: 'eth_requestAccounts' })}>Enable Ethereum</button> */}
+      <div className="row">
+        <div className="col-sm-12">
           <h1>Crud</h1>
-          <form id="create">
+          <form id="create" onSubmit={handleCreate}>
             <h2>Create user</h2>
-            <div class="form-group">
+            <div className="form-group">
               <label for="name">Name</label>
-              <input id="name" type="text" class="form-control"></input>
+              <input id="name" name='createName' type="text" className="form-control"
+              onChange={handleInputChange} value={input.createName}
+              >
+              </input>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Submit</button>
             <p id="create-result"></p>
           </form>
 
 
           <form id="read">
             <h2>Read user</h2>
-            <div class="form-group">
+            <div className="form-group">
               <label for="read-id">Id</label>
-              <input id="read-id" type="number" class="form-control"></input>
+              <input id="read-id" name='readId'type="number" className="form-control"
+              onChange={handleInputChange} value={input.readId}
+              ></input>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Submit</button>
             <p id="read-result"></p>
           </form>
 
           <form id="edit">
             <h2>Edit user</h2>
-            <div class="form-group">
+            <div className="form-group">
               <label for="edit-id">Id</label>
-              <input id="edit-id" type="number" class="form-control"></input>
+              <input id="edit-id" name='editId' type="number" className="form-control"
+              onChange={handleInputChange} value={input.editId}
+              ></input>
               <label for="edit-name">Name</label>
-              <input id="edit-name" type="text" class="form-control"></input>
+              <input id="edit-name" name='editName'type="text" className="form-control"
+              onChange={handleInputChange} value={input.editName}
+              ></input>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Submit</button>
             <p id="edit-result"></p>
           </form>
 
           <form id="delete">
             <h2>Delete user</h2>
-            <div class="form-group">
+            <div className="form-group">
               <label for="delete-id">Id</label>
-              <input id="delete-id" type="number" class="form-control"></input>
+              <input id="delete-id" name='deleteId'type="number" className="form-control"
+              onChange={handleInputChange} value={input.deleteId}
+              ></input>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Submit</button>
             <p id="delete-result"></p>
           </form>
 
